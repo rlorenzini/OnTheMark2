46633a457698ff30761f0ff857fee0cada59f619
amehta27 <apmk_27@yahoo.com>
2019-04-08 12:21:44 -0500

    leaflet in public

--- app.js
+++ app.js
@@ -4,22 +4,35 @@ const express = require('express'),
   server = express(),
   port = 3000,
   path = require('path'),
-  leafletRoutes = require('./routes/leaflet'),
+  router = express.Router(),
   VIEWS_PATH = path.join(__dirname, '/views');
+
+
 
 console.log(VIEWS_PATH)
 var cors = require('cors')
 server.use(cors())
+server.use(express.static("public"))
+
 server.use(bodyParser.urlencoded({extended:false}))
-server.use('/leaflet', leafletRoutes)
+// server.use('/leaflet', leafletRoutes)
 server.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
 server.set('views','./views')
 server.set('view engine','mustache')
 
 let storedCoordinates = []
-
 server.get('/',(req,res)=>{
-  res.render('index')
+  res.render('register')
 })
+
+// server.get('/register',(req,res)=> {
+//   res.render("register")
+
+// })
+
+
+
+
+
 
 server.listen(port,()=>{console.log(`Server is running on port ${port}.`)})
