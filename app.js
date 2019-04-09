0059dd3759cf389ea8db3dc31acc4c0c89dee9dc
Mike Starbuck <miglas9@gmail.com>
2019-04-09 09:45:53 -0500

    user auth functionality

--- app.js
+++ app.js
@@ -17,57 +17,88 @@ var cors = require('cors')
 server.use(cors())
 server.use(express.static("public"))
 
-server.use(bodyParser.urlencoded({extended:false}))
+server.use(bodyParser.urlencoded({ extended: false }))
 // server.use('/leaflet', leafletRoutes)
-server.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
-server.set('views','./views')
-server.set('view engine','mustache')
+server.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
+server.set('views', './views')
+server.set('view engine', 'mustache')
 
 let storedCoordinates = []
-server.get('/register',(req,res)=>{
+server.get('/register', (req, res) => {
   res.render('register')
 })
 
-server.post('/register',(req,res)=>{
+server.post('/register', (req, res) => {
   console.log("Hello")
   let username = req.body.username
   let password = req.body.password
 
   models.User.findOne({
-    where :{username : username,}
-  }).then((user)=>{
-    if (user){
+    where: { username: username, }
+  }).then((user) => {
+    if (user) {
       console.log("failed")
-      res.render('register',{message: "User name already exists!"})
+      res.render('register', { message: "User name already exists!" })
     } else {
-      bcrypt.hash(password,SALT_ROUNDS, function(error, hash){
-        if(error == null) {
+      bcrypt.hash(password, SALT_ROUNDS, function (error, hash) {
+        if (error == null) {
           let user = models.User.build({
-            username : username,
-            password : hash
-        }) 
-        user.save().then((savedUser)=>{
-          console.log(savedUser)
-        }).then(() => {
-          res.redirect('/login')
-        })    
-    
+            username: username,
+            password: hash
+          })
+          user.save().then((savedUser) => {
+            console.log(savedUser)
+          }).then(() => {
+            res.redirect('/login')
+          })
+
         }
       })
-    
-  }
-  
+
+    }
+
   })
 })
 
-server.get('/login',(req,res)=> {
+server.get('/login', (req, res) => {
   res.render("login")
 
+})
+//============ New Authorization code =============
+server.post('/login', (req, res) => {
+
+  let username = req.body.username
+  let password = req.body.password
+
+  models.User.findOne({
+    where: {
+      username: username
+    }
+  }).then((user) => {
+    console.log(user.password)
+    if (user) { //check for user password
+      bcrypt.compare(password, user.password, (error, result) => {
+        if (result) {
+          // check for admin 
+          // if (user.admin == true) // render admin page
+          res.render('leaflet')
+        } else {
+          res.render('login', { message: "Invalid username or password." })
+        }
+      })
+    }
+  }).catch(() => {
+    res.render('login', { message: "Invalid username or password." })
+  })
+
+})
+
+//logout code
+server.post('/logout', (req, res) => {
+  res.destroy()
 })
 
 
 
 
-
-
-server.listen(port,()=>{console.log(`Server is running on port ${port}.`)})
+server.listen(port, () => { console.log(`Server is running on port ${port}.`) })
